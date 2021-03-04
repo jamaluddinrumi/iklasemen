e9ea59a77d8f43cd413f95cf2d4972dc087e92eb
Jamaluddin Rumi <jamal.rumi@icloud.com>
2021-03-04 23:44:11 +0700

    Build production

--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,1 @@
-function onSubmit() {
-    const submitButton = document.getElementById("submit-button");
-    const spinningIcon = `<span class="v-btn__loader"><div role="progressbar" aria-valuemin="0" aria-valuemax="100" class="v-progress-circular v-progress-circular--indeterminate" style="height: 23px; width: 23px;"><svg xmlns="http://www.w3.org/2000/svg" viewBox="21.904761904761905 21.904761904761905 43.80952380952381 43.80952380952381" style="transform: rotate(0deg);"><circle fill="transparent" cx="43.8095238095238
-    1" cy="43.80952380952381" r="20" stroke-width="10" stroke-dasharray="125.664" stroke-dashoffset="125.66370614359172px" class="v-progress-circular__overlay"></circle></svg><div class="v-progress-circular__info"></div></div></span>`;
-
-    submitButton.classList.add("v-btn--loading");
-    submitButton.insertAdjacentHTML("beforeend", spinningIcon);
-}
+function onSubmit(){const r=document.getElementById("submit-button");r.classList.add("v-btn--loading"),r.insertAdjacentHTML("beforeend",'<span class="v-btn__loader"><div role="progressbar" aria-valuemin="0" aria-valuemax="100" class="v-progress-circular v-progress-circular--indeterminate" style="height: 23px; width: 23px;"><svg xmlns="http://www.w3.org/2000/svg" viewBox="21.904761904761905 21.904761904761905 43.80952380952381 43.80952380952381" style="transform: rotate(0deg);"><circle fill="transparent" cx="43.8095238095238\n    1" cy="43.80952380952381" r="20" stroke-width="10" stroke-dasharray="125.664" stroke-dashoffset="125.66370614359172px" class="v-progress-circular__overlay"></circle></svg><div class="v-progress-circular__info"></div></div></span>')}
